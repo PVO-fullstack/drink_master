8a8509d914ff744956eef904d28dd1f1998df5a0
Viacheslav Plevako <cheryec@ukr.net>
2023-08-24 17:09:17 +0300

    change build

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,3 @@
-// import { Link, Route, Routes } from "react-router-dom";
-// import Home from "./pages/Home";
-// import { Drinks } from "./pages/Drinks";
-// import { AddRecipe } from "./pages/AddRecipe";
-// import { MyRecipes } from "./pages/MyRecipes";
-// import { Favorites } from "./pages/Favorites";
 import { Navigation } from "./components/Navigation/Navigation";
 import { UserRoutes } from "./UserRoutes";
 
