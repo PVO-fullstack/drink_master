1f4da5aae269da56d22356d3a7c2c4d5f98d96dc
dev0652 <113797010+dev0652@users.noreply.github.com>
2023-08-25 16:37:18 +0300

    tmp

--- src/components/AddRecipeForm/AddRecipeForm.jsx
+++ src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -1,10 +1,26 @@
 // import React from 'react'
 // import PropTypes from "prop-types";
+import { Formik, Form, Field } from 'formik';
 
 // ###################################################
 
 export const AddRecipeForm = () => {
-  return <div>tmp</div>;
+  return (
+    <Formik>
+      <Form
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 5,
+          alignItems: 'start',
+        }}
+      >
+        <Field />
+        <Field />
+        <Field />
+      </Form>
+    </Formik>
+  );
 };
 
 // AddRecipeForm.propTypes = {
