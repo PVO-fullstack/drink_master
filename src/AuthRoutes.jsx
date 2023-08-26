d006150396720861c2ae13e828cfddccad6825f5
Viacheslav Plevako <cheryec@ukr.net>
2023-08-26 13:50:12 +0300

    add auth form

--- src/AuthRoutes.jsx
+++ src/AuthRoutes.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import { Start } from "./pages/AuthPages/Start";
 import { Registration } from "./pages/AuthPages/Registration";
-import { SignIn } from "./pages/AuthPages/SignIn";
+import { Login } from "./pages/AuthPages/Login";
 
 export const AuthRoutes = () => {
   return (
     <Routes>
       <Route path="/welcome" element={<Start />} />
       <Route path="/signup" element={<Registration />} />
-      <Route path="/signin" element={<SignIn />} />
+      <Route path="/signin" element={<Login />} />
     </Routes>
   );
 };
