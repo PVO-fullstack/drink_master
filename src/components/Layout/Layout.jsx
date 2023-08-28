88185522936fba943bf084d6e3febb2ce8b69bbc
Viacheslav Plevako <cheryec@ukr.net>
2023-08-28 07:57:59 +0300

    Update Layout.jsx

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,14 +1,15 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { Header } from "../Header/Header";
 import Footer from "../Footer/Footer";
+import { Outlet } from "react-router";
 
-
-export const Layout = ({ children }) => {
+export const Layout = () => {
   return (
     <div>
       <Header />
-      {children}
+      <main>
+        <Outlet />
+      </main>
       <Footer />
     </div>
   );
