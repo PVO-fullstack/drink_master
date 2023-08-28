c1564338c710ec1199de81d77938b42f8a2301b3
Marhosius <marhosius@gmail.com>
2023-08-28 20:30:57 +0300

    Burger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navigation } from "../Navigation/Navigation";
 import UserMenu from "../UserMenu/UserMenu";
 import Logo from "../Logo/Logo";
 import css from './Header.module.scss'
 
 export const Header = () => {
+  const [BurgerNavigation, setBurgerNavigation] = useState(false);
+  const togleBurgerBackdrop = (e) => {
+    setBurgerNavigation(!BurgerNavigation)
+    e.target.classList.toggle("active")
+  };
+  const burgerPosition = BurgerNavigation ? "84px" : "-100vh"
+
+
   return (
     <header className={css.header}>
       <div className={css.headerContainer}>
         <Logo />
         <Navigation style="csshead" />
-        <UserMenu />
+        <div className={css.headerSideWrapper}>
+          <UserMenu />
+          <button onClick={togleBurgerBackdrop} className={css.headerBurgerMenuBTN}>
+            <img
+              src={BurgerNavigation ? "/images/SVG/close.svg" : "/images/SVG/align-justify.svg"}
+              alt="Navigation Icon"
+              className={css.headerBurgerMenuIcon}
+              width={32}
+              height={32}
+            ></img>
+          </button>
+        </div>
+      </div>
+      <div className={css.headerBurgerBackdrop} style={{ top: burgerPosition }}>
+        <Navigation style="cssBurger" />
       </div>
       <div className={css.headerUnderline}></div>
     </header>
