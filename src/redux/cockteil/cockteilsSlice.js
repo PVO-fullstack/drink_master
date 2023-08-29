63e2e9b0c741eaa572b8c2726016a549bf0b3644
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-08-29 22:36:43 +0300

    paginators

--- src/redux/cockteil/cockteilsSlice.js
+++ src/redux/cockteil/cockteilsSlice.js
@@ -1,35 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { fetchRecipesThunk } from "./cockteilsOperations";
-// import {
-//   fetchFavoriteRecipesThunk,
-//   fetchMyRecipesThunk,
-// } from "./cockteilsOperations";
-
-// const initialState = { myRecipes: [], favRecipes: [], error: null };
-
-// export const cockteilsSlise = createSlice({
-//   name: "recipes",
-//   initialState,
-//   reducers: {},
-
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(fetchMyRecipesThunk.fulfilled, (state, { payload }) => {
-//         state.myRecipes = payload;
-//         state.error = null;
-//       })
-//       .addCase(fetchFavoriteRecipesThunk.fulfilled, (state, { payload }) => {
-//         state.favRecipes = payload;
-//         state.error = null;
-//       })
-//       .addMatcher(
-//         (action) => action.type.endsWith("/rejected"),
-//         (state, { payload }) => {
-//           state.error = payload;
-//         }
-//       );
-//   },
-// });
 
 const initialState = {
   myRecipes: [],
