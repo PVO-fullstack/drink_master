4506ecd966f80258caacba14eba7cfa4773b1ae7
Viacheslav Plevako <cheryec@ukr.net>
2023-08-30 11:25:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/drink_master">
           <App />
         </BrowserRouter>
       </PersistGate>
