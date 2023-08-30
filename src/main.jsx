1f6dc8307d8b61988b020818fa14af130353c82c
Viacheslav Plevako <cheryec@ukr.net>
2023-08-30 12:56:40 +0300

    change routing

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import "./styles/reset.scss";
 import "./styles/styles.scss";
-import { RouterProvider, createHashRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createHashRouter,
+} from "react-router-dom";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/*",
     element: <App />,
