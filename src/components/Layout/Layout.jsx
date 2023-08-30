0179a70929c79ef434aa8cac6f05816c1a150a5a
Viacheslav Plevako <cheryec@ukr.net>
2023-08-30 22:53:20 +0300

    change pagePadding

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -2,12 +2,13 @@ import PropTypes from "prop-types";
 import { Header } from "../Header/Header";
 import Footer from "../Footer/Footer";
 import { Outlet } from "react-router";
+import style from "./Layout.module.scss";
 
 export const Layout = () => {
   return (
     <div>
       <Header />
-      <main>
+      <main className={style.pagePadding}>
         <Outlet />
       </main>
       <Footer />
