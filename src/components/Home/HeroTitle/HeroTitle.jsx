405ac9e59e74814089071a1a10191ff334d50e88
DmytroSkoryk <114485103+DmytroSkoryk@users.noreply.github.com>
2023-08-31 23:24:35 +0300

    first attempt

---
+++ src/components/Home/HeroTitle/HeroTitle.jsx
@@ -0,0 +1,12 @@
+import css from "../HeroTitle/HeroTitle.module.scss";
+
+const HeroTitle = () => {
+  return (
+    <div>
+      <h1 className={css.heroTitle}>
+        Craft Your Perfect Drink with Drink Master
+      </h1>
+    </div>
+  );
+};
+export default HeroTitle;
