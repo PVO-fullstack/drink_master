416ab20f16023ff05000a64043e9a01a40dff4f8
Viacheslav Plevako <cheryec@ukr.net>
2023-09-01 14:37:00 +0300

    change UserModal

--- src/components/UserLogoModal/UserLogoModal.jsx
+++ src/components/UserLogoModal/UserLogoModal.jsx
@@ -1,61 +1,36 @@
 import style from "./UserLogoModal.module.scss";
-import { useDispatch } from "react-redux";
-import { logOutUser } from "../../redux/auth/authOperations";
-import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import editSVG from "/images/SVG/edit.svg";
-import { UserButton } from "../UserButton/UserButton";
-import { UserInfoModal } from "../UserInfoModal/UserInfoModal";
+import { useDispatch } from "react-redux";
+import { logOutUser } from "../../redux/auth/authOperations";
 
-export const UserLogoModal = ({ closeUserMenu }) => {
-  const [edit, setEdit] = useState(false);
-
-  useEffect(() => {
-    console.log("first");
-
-    return () => {
-      console.log("second");
-    };
-  }, []);
-
-  useEffect(() => {
-    console.log("first");
-    () => {
-      console.log("second");
-      closeUserMenu();
-    };
-  });
-
+export const UserLogoModal = ({ close, showUserInfo }) => {
   const dispatch = useDispatch();
 
-  const handleLogout = () => {
+  const showUser = () => {
+    showUserInfo();
+    close();
+  };
+
+  const logqOut = () => {
     dispatch(logOutUser());
-  };
-
-  const handleEditClick = () => {
-    setEdit(true);
-  };
-
-  const handleCloseModal = () => {
-    setEdit(false);
-    closeUserMenu();
   };
 
   return (
     <>
-      {createPortal(
-        (!edit && (
-          <UserButton change={handleEditClick} logout={handleLogout} />
-        )) ||
-          (edit && (
-            <UserInfoModal
-              close={handleCloseModal}
-              isEdit={edit}
-              closeUser={closeUserMenu}
-            />
-          )),
-        document.getElementById("modal-root")
-      )}
+      <div className={style.backdrop}>
+        <div className={style.modal}>
+          <div className={style.btnConteiner}>
+            <button onClick={showUser} className={style.edit} type="button">
+              Edit profile
+              <img className={style.edit_swg} src={editSVG} alt="edit" />
+            </button>
+            <button onClick={logqOut} className={style.btn} type="button">
+              Log out
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
