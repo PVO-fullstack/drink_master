ab5a1d419798294a14fb3b978004ed82c758b6e7
Marhosius <marhosius@gmail.com>
2023-09-01 13:02:22 +0300

    Supscride-addet(no tests)

--- src/components/SubscribeForm/SubscribeForm.jsx
+++ src/components/SubscribeForm/SubscribeForm.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import css from './SubscribeForm.module.scss'
+import { useDispatch } from 'react-redux';
+import { subscribeUser } from '../../redux/auth/authOperations';
 
 const SubscribeForm = () => {
+    const dispatch = useDispatch();
     const emailRegexp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
-    const SignupSchema = Yup.object({
+    const EmailSchema = Yup.object({
         email: Yup.string()
             .matches(emailRegexp)
             .required("Email required")
@@ -13,25 +16,30 @@ const SubscribeForm = () => {
     return (
         <Formik
             initialValues={{ email: "" }}
-            validationSchema={SignupSchema}
+            validationSchema={EmailSchema}
             onSubmit={async (values, { resetForm }) => {
-                console.log(values);
-                // dispatch(registerUser(values));
+                dispatch(subscribeUser(values));
                 resetForm();
             }}
         >
             {(formik) => {
-                const { isValid } = formik;
+                const { isValid, dirty } = formik;
                 return (
                     <Form className={css.SubscribeForm} >
-                        <p className={css.SubscribeFormText}>Subscribe up to our newsletter. Be in touch with latest news and special offers, etc.</p>
+                        <p className={css.SubscribeFormText}
+                        >Subscribe up to our newsletter. Be in touch with latest news and special offers, etc.
+                        </p>
                         <Field
                             className={css.SubscribeFormInput}
                             type="email"
                             name="email"
                             placeholder="Email"
                         />
-                        <button disabled={!isValid} className={css.SubscribeFormSubmit} type='submit'>Subscribe</button>
+                        <button
+                            disabled={!(dirty && isValid)}
+                            className={css.SubscribeFormSubmit}
+                            type='submit'>Subscribe
+                        </button>
                     </Form>
                 );
             }}
