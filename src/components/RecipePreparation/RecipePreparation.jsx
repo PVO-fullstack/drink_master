d881146801ff6d8faa44afa976be53d0cce78853
Inna Nomerovkina <inkosof@gmail.com>
2023-09-01 18:58:52 +0300

    Update RecipePreparation.jsx

--- src/components/RecipePreparation/RecipePreparation.jsx
+++ src/components/RecipePreparation/RecipePreparation.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import css from "./RecipePreparation.module.scss"
 
 
+
 export const RecipePreparation = ({instructions}) => {
 
     return (
         <div className={css.preparation}>
             <h2 className={ css.prep_title} >Recipe Preparation</h2>
-            {/* <img src="" alt="Coctails" /> */}
-            <p>{instructions}</p>
+            <div className={css.prep}>
+                <img className={ css.prep_img} src="../public/recipePrep.jpg" alt="Coctails" />
+            
+            <ul className={css.prep_list}>
+                {instructions.map(item => (
+                    <li className={css.prep_item}>
+                        {item}
+                    </li>
+                ))
+                }
+                </ul>
+            </div>
         </div>
         
         
