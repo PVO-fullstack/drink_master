6319bb7c3a5c56287a697ef58a38b47353c9365a
Viacheslav Plevako <cheryec@ukr.net>
2023-09-02 13:36:28 +0300

    add change avatar

--- src/components/UserInfoModal/UserInfoModal.jsx
+++ src/components/UserInfoModal/UserInfoModal.jsx
@@ -1,21 +1,38 @@
 import style from "./UserInfoModal.module.scss";
 import closeSVG from "/images/SVG/x.svg";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectUser } from "../../redux/auth/authSelectors";
 import addPhoto from "/images/SVG/add_photo.svg";
 import { Field, Form, Formik } from "formik";
 import { Modal } from "../Modal/Modal";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { changeAvatar } from "../../redux/auth/authOperations";
 
 export const UserInfoModal = ({ close, closeModal }) => {
   const { name, avatarURL } = useSelector(selectUser);
+  const [imgSrc, setImgSrc] = useState(null);
+  const [avatar, setAvatar] = useState(null);
+
+  const dispatch = useDispatch();
 
   const closeInfo = () => {
     closeModal();
   };
 
-  const handleChangeAvatar = () => {
-    console.log("first");
+  const changeFile = (event) => {
+    const target = event.target;
+
+    if (!target) {
+      alert("Ничего не загружено");
+      return;
+    }
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setAvatar(fileReader.result);
+    };
+
+    fileReader.readAsDataURL(target.files[0]);
   };
 
   useEffect(() => {
@@ -32,38 +49,60 @@ export const UserInfoModal = ({ close, closeModal }) => {
         alt="close"
       />
 
-      <div onClick={handleChangeAvatar} className={style.avatarConteiner}>
+      {/* <div onClick={handleChangeAvatar} className={style.avatarConteiner}>
         <img
-          className={style.avatar}
-          src={avatarURL}
-          alt="Avatar"
-          width={100}
-          height={100}
+        className={style.avatar}
+        src={avatarURL}
+        alt="Avatar"
+        width={100}
+        height={100}
         />
-        <img className={style.addPhoto} src={addPhoto} alt="add photo" />
-      </div>
+        
+      </div> */}
       <Formik
-        initialValues={{ name: `${name}` }}
+        initialValues={{ name: `${name}`, avatar: "" }}
         // validationSchema={SignupSchema}
-        // onSubmit={async (values, { resetForm }) => {
-        //   console.log(values);
-        //   dispatch(logInUser(values));
-        //   resetForm();
-        // }}
+        onSubmit={async (values, { resetForm }) => {
+          // const formData = new FormData();
+          // formData.append("userpic", imgSrc, "avatar.jpg");
+          // formData.append(values);
+          // console.log("AAAAAA", values);
+
+          dispatch(changeAvatar(values));
+          resetForm();
+        }}
       >
         {(formik) => {
-          const { isValid, dirty } = formik;
+          const { isValid, dirty, setFieldValue } = formik;
           return (
-            <Form className={style.form}>
-              <label className={style.img}>
-                <input
-                  type="file"
-                  name="avatar"
-                  // onChange={(event) => {
-                  //   setFieldValue("avatar", event.currentTarget.files[0]);
-                  // }}
+            <Form className={imgSrc ? style.foto : style.form}>
+              {imgSrc ? (
+                <img
+                  className={style.avatarImg}
+                  src={avatar}
+                  alt="Avatar"
+                  width={100}
+                  height={100}
                 />
-              </label>
+              ) : (
+                <div className={style.avatarConteiner}>
+                  <Field
+                    className={style.avatar}
+                    type="file"
+                    name="avatar"
+                    onChange={(e) => {
+                      setFieldValue("avatar", e.currentTarget.files[0]);
+                      setImgSrc(e.currentTarget.files[0]);
+                      changeFile(e);
+                    }}
+                  />
+                  <img
+                    className={style.addPhoto}
+                    src={addPhoto}
+                    alt="add photo"
+                  />
+                </div>
+              )}
               <Field
                 className={style.field}
                 type="text"
@@ -74,9 +113,8 @@ export const UserInfoModal = ({ close, closeModal }) => {
                 // disabled={!(dirty && isValid)}
                 className={style.btn}
                 type="submit"
-                onClick={() => logOut()}
               >
-                Sign In
+                Save changes
               </button>
             </Form>
           );
