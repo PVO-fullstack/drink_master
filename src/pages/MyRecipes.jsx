150531277800e4cbcf81bf44561d37394991a391
YuraBuchak <115108617+YuraBuchak@users.noreply.github.com>
2023-09-02 11:38:52 +0300

    сss page fav-myRec

--- src/pages/MyRecipes.jsx
+++ src/pages/MyRecipes.jsx
@@ -1,9 +1,10 @@
 import { RecipesList } from "../components";
 import { PageTitle } from "../components/Typography/PageTitle/PageTitle";
+import css from "../components/RecipesList/RecipesList.module.scss";
 
 export const MyRecipes = () => {
   return (
-    <div>
+    <div className={css.container}>
       <PageTitle>MyRecipes</PageTitle>
       <RecipesList type="own" />
     </div>
